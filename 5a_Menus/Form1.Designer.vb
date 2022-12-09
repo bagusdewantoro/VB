403cd317b88e7c2e3e36e40d3c7aebf52a233c8a
bagusdewantoro <bagusdewantoro86@gmail.com>
2022-12-09 17:22:19 +0700

    menus: open, close, edit, hide

--- 5a_Menus/Form1.Designer.vb
+++ 5a_Menus/Form1.Designer.vb
@@ -22,6 +22,7 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
         Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
         Me.FileToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
         Me.menuNew = New System.Windows.Forms.ToolStripMenuItem()
@@ -32,19 +33,23 @@ Partial Class Form1
         Me.ToolStripMenuItem1 = New System.Windows.Forms.ToolStripSeparator()
         Me.menuExit = New System.Windows.Forms.ToolStripMenuItem()
         Me.EditToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
-        Me.UndoToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
-        Me.CutToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
-        Me.CopyToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
-        Me.PasteToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.menuUndo = New System.Windows.Forms.ToolStripMenuItem()
+        Me.menuCut = New System.Windows.Forms.ToolStripMenuItem()
+        Me.menuCopy = New System.Windows.Forms.ToolStripMenuItem()
+        Me.menuPaste = New System.Windows.Forms.ToolStripMenuItem()
         Me.ViewToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
-        Me.ViewTextBoxesToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
-        Me.ViewLabelsToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
-        Me.ViewImagesToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.menuViewTextBoxes = New System.Windows.Forms.ToolStripMenuItem()
+        Me.menuViewLabels = New System.Windows.Forms.ToolStripMenuItem()
+        Me.menuViewImages = New System.Windows.Forms.ToolStripMenuItem()
         Me.TextBox1 = New System.Windows.Forms.TextBox()
         Me.TextBox2 = New System.Windows.Forms.TextBox()
         Me.openFD = New System.Windows.Forms.OpenFileDialog()
         Me.saveFD = New System.Windows.Forms.SaveFileDialog()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
         Me.MenuStrip1.SuspendLayout()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
         'MenuStrip1
@@ -53,7 +58,7 @@ Partial Class Form1
         Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.FileToolStripMenuItem, Me.EditToolStripMenuItem, Me.ViewToolStripMenuItem})
         Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
         Me.MenuStrip1.Name = "MenuStrip1"
-        Me.MenuStrip1.Size = New System.Drawing.Size(800, 28)
+        Me.MenuStrip1.Size = New System.Drawing.Size(619, 30)
         Me.MenuStrip1.TabIndex = 0
         Me.MenuStrip1.Text = "MenuStrip1"
         '
@@ -68,7 +73,7 @@ Partial Class Form1
         '
         Me.menuNew.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.NewProjectToolStripMenuItem, Me.NewFileToolStripMenuItem})
         Me.menuNew.Name = "menuNew"
-        Me.menuNew.Size = New System.Drawing.Size(224, 26)
+        Me.menuNew.Size = New System.Drawing.Size(181, 26)
         Me.menuNew.Text = "&New"
         '
         'NewProjectToolStripMenuItem
@@ -87,127 +92,165 @@ Partial Class Form1
         '
         Me.menuOpen.Name = "menuOpen"
         Me.menuOpen.ShortcutKeys = CType((System.Windows.Forms.Keys.Control Or System.Windows.Forms.Keys.O), System.Windows.Forms.Keys)
-        Me.menuOpen.Size = New System.Drawing.Size(224, 26)
+        Me.menuOpen.Size = New System.Drawing.Size(181, 26)
         Me.menuOpen.Text = "&Open"
         '
         'menuSave
         '
         Me.menuSave.Name = "menuSave"
         Me.menuSave.ShortcutKeys = CType((System.Windows.Forms.Keys.Control Or System.Windows.Forms.Keys.S), System.Windows.Forms.Keys)
-        Me.menuSave.Size = New System.Drawing.Size(224, 26)
+        Me.menuSave.Size = New System.Drawing.Size(181, 26)
         Me.menuSave.Text = "&Save"
         '
         'ToolStripMenuItem1
         '
         Me.ToolStripMenuItem1.Name = "ToolStripMenuItem1"
-        Me.ToolStripMenuItem1.Size = New System.Drawing.Size(221, 6)
+        Me.ToolStripMenuItem1.Size = New System.Drawing.Size(178, 6)
         '
         'menuExit
         '
         Me.menuExit.Name = "menuExit"
         Me.menuExit.Overflow = System.Windows.Forms.ToolStripItemOverflow.Always
-        Me.menuExit.ShortcutKeys = CType((System.Windows.Forms.Keys.Control Or System.Windows.Forms.Keys.X), System.Windows.Forms.Keys)
-        Me.menuExit.Size = New System.Drawing.Size(224, 26)
+        Me.menuExit.ShortcutKeys = CType((System.Windows.Forms.Keys.Control Or System.Windows.Forms.Keys.W), System.Windows.Forms.Keys)
+        Me.menuExit.Size = New System.Drawing.Size(181, 26)
         Me.menuExit.Text = "E&xit"
         '
         'EditToolStripMenuItem
         '
-        Me.EditToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.UndoToolStripMenuItem, Me.CutToolStripMenuItem, Me.CopyToolStripMenuItem, Me.PasteToolStripMenuItem})
+        Me.EditToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.menuUndo, Me.menuCut, Me.menuCopy, Me.menuPaste})
         Me.EditToolStripMenuItem.Name = "EditToolStripMenuItem"
         Me.EditToolStripMenuItem.Size = New System.Drawing.Size(49, 24)
         Me.EditToolStripMenuItem.Text = "&Edit"
         '
-        'UndoToolStripMenuItem
+        'menuUndo
         '
-        Me.UndoToolStripMenuItem.Name = "UndoToolStripMenuItem"
-        Me.UndoToolStripMenuItem.ShortcutKeys = CType((System.Windows.Forms.Keys.Control Or System.Windows.Forms.Keys.B), System.Windows.Forms.Keys)
-        Me.UndoToolStripMenuItem.Size = New System.Drawing.Size(179, 26)
-        Me.UndoToolStripMenuItem.Text = "&Undo"
+        Me.menuUndo.Name = "menuUndo"
+        Me.menuUndo.ShortcutKeys = CType((System.Windows.Forms.Keys.Control Or System.Windows.Forms.Keys.B), System.Windows.Forms.Keys)
+        Me.menuUndo.Size = New System.Drawing.Size(224, 26)
+        Me.menuUndo.Text = "&Undo"
         '
-        'CutToolStripMenuItem
+        'menuCut
         '
-        Me.CutToolStripMenuItem.Name = "CutToolStripMenuItem"
-        Me.CutToolStripMenuItem.ShortcutKeys = CType((System.Windows.Forms.Keys.Control Or System.Windows.Forms.Keys.X), System.Windows.Forms.Keys)
-        Me.CutToolStripMenuItem.Size = New System.Drawing.Size(179, 26)
-        Me.CutToolStripMenuItem.Text = "Cu&t"
+        Me.menuCut.Name = "menuCut"
+        Me.menuCut.ShortcutKeys = CType((System.Windows.Forms.Keys.Control Or System.Windows.Forms.Keys.X), System.Windows.Forms.Keys)
+        Me.menuCut.Size = New System.Drawing.Size(224, 26)
+        Me.menuCut.Text = "Cu&t"
         '
-        'CopyToolStripMenuItem
+        'menuCopy
         '
-        Me.CopyToolStripMenuItem.Name = "CopyToolStripMenuItem"
-        Me.CopyToolStripMenuItem.ShortcutKeys = CType((System.Windows.Forms.Keys.Control Or System.Windows.Forms.Keys.C), System.Windows.Forms.Keys)
-        Me.CopyToolStripMenuItem.Size = New System.Drawing.Size(179, 26)
-        Me.CopyToolStripMenuItem.Text = "&Copy"
+        Me.menuCopy.Name = "menuCopy"
+        Me.menuCopy.ShortcutKeys = CType((System.Windows.Forms.Keys.Control Or System.Windows.Forms.Keys.C), System.Windows.Forms.Keys)
+        Me.menuCopy.Size = New System.Drawing.Size(224, 26)
+        Me.menuCopy.Text = "&Copy"
         '
-        'PasteToolStripMenuItem
+        'menuPaste
         '
-        Me.PasteToolStripMenuItem.Name = "PasteToolStripMenuItem"
-        Me.PasteToolStripMenuItem.ShortcutKeyDisplayString = ""
-        Me.PasteToolStripMenuItem.ShortcutKeys = CType((System.Windows.Forms.Keys.Control Or System.Windows.Forms.Keys.V), System.Windows.Forms.Keys)
-        Me.PasteToolStripMenuItem.Size = New System.Drawing.Size(179, 26)
-        Me.PasteToolStripMenuItem.Text = "&Paste"
+        Me.menuPaste.Name = "menuPaste"
+        Me.menuPaste.ShortcutKeyDisplayString = ""
+        Me.menuPaste.ShortcutKeys = CType((System.Windows.Forms.Keys.Control Or System.Windows.Forms.Keys.V), System.Windows.Forms.Keys)
+        Me.menuPaste.Size = New System.Drawing.Size(224, 26)
+        Me.menuPaste.Text = "&Paste"
         '
         'ViewToolStripMenuItem
         '
-        Me.ViewToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ViewTextBoxesToolStripMenuItem, Me.ViewLabelsToolStripMenuItem, Me.ViewImagesToolStripMenuItem})
+        Me.ViewToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.menuViewTextBoxes, Me.menuViewLabels, Me.menuViewImages})
         Me.ViewToolStripMenuItem.Name = "ViewToolStripMenuItem"
         Me.ViewToolStripMenuItem.Size = New System.Drawing.Size(55, 24)
         Me.ViewToolStripMenuItem.Text = "&View"
         '
-        'ViewTextBoxesToolStripMenuItem
+        'menuViewTextBoxes
         '
-        Me.ViewTextBoxesToolStripMenuItem.Name = "ViewTextBoxesToolStripMenuItem"
-        Me.ViewTextBoxesToolStripMenuItem.ShortcutKeys = CType(((System.Windows.Forms.Keys.Control Or System.Windows.Forms.Keys.Shift) _
+        Me.menuViewTextBoxes.Name = "menuViewTextBoxes"
+        Me.menuViewTextBoxes.ShortcutKeys = CType(((System.Windows.Forms.Keys.Control Or System.Windows.Forms.Keys.Shift) _
             Or System.Windows.Forms.Keys.T), System.Windows.Forms.Keys)
-        Me.ViewTextBoxesToolStripMenuItem.Size = New System.Drawing.Size(288, 26)
-        Me.ViewTextBoxesToolStripMenuItem.Text = "View Text &Boxes"
+        Me.menuViewTextBoxes.Size = New System.Drawing.Size(288, 26)
+        Me.menuViewTextBoxes.Text = "View Text &Boxes"
         '
-        'ViewLabelsToolStripMenuItem
+        'menuViewLabels
         '
-        Me.ViewLabelsToolStripMenuItem.Name = "ViewLabelsToolStripMenuItem"
-        Me.ViewLabelsToolStripMenuItem.ShortcutKeyDisplayString = ""
-        Me.ViewLabelsToolStripMenuItem.ShortcutKeys = CType(((System.Windows.Forms.Keys.Control Or System.Windows.Forms.Keys.Shift) _
+        Me.menuViewLabels.Name = "menuViewLabels"
+        Me.menuViewLabels.ShortcutKeyDisplayString = ""
+        Me.menuViewLabels.ShortcutKeys = CType(((System.Windows.Forms.Keys.Control Or System.Windows.Forms.Keys.Shift) _
             Or System.Windows.Forms.Keys.L), System.Windows.Forms.Keys)
-        Me.ViewLabelsToolStripMenuItem.Size = New System.Drawing.Size(288, 26)
-        Me.ViewLabelsToolStripMenuItem.Text = "View &Labels"
+        Me.menuViewLabels.Size = New System.Drawing.Size(288, 26)
+        Me.menuViewLabels.Text = "View &Labels"
         '
-        'ViewImagesToolStripMenuItem
+        'menuViewImages
         '
-        Me.ViewImagesToolStripMenuItem.Name = "ViewImagesToolStripMenuItem"
-        Me.ViewImagesToolStripMenuItem.ShortcutKeys = CType(((System.Windows.Forms.Keys.Control Or System.Windows.Forms.Keys.Shift) _
+        Me.menuViewImages.Name = "menuViewImages"
+        Me.menuViewImages.ShortcutKeys = CType(((System.Windows.Forms.Keys.Control Or System.Windows.Forms.Keys.Shift) _
             Or System.Windows.Forms.Keys.I), System.Windows.Forms.Keys)
-        Me.ViewImagesToolStripMenuItem.Size = New System.Drawing.Size(288, 26)
-        Me.ViewImagesToolStripMenuItem.Text = "View &Images"
+        Me.menuViewImages.Size = New System.Drawing.Size(288, 26)
+        Me.menuViewImages.Text = "View &Images"
         '
         'TextBox1
         '
         Me.TextBox1.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.0!)
         Me.TextBox1.ImeMode = System.Windows.Forms.ImeMode.NoControl
-        Me.TextBox1.Location = New System.Drawing.Point(30, 110)
+        Me.TextBox1.Location = New System.Drawing.Point(32, 86)
         Me.TextBox1.Multiline = True
         Me.TextBox1.Name = "TextBox1"
         Me.TextBox1.Size = New System.Drawing.Size(227, 183)
         Me.TextBox1.TabIndex = 1
         Me.TextBox1.Text = "Default Text"
+        Me.TextBox1.Visible = False
         '
         'TextBox2
         '
         Me.TextBox2.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.0!)
         Me.TextBox2.ImeMode = System.Windows.Forms.ImeMode.NoControl
-        Me.TextBox2.Location = New System.Drawing.Point(340, 110)
+        Me.TextBox2.Location = New System.Drawing.Point(342, 86)
         Me.TextBox2.Multiline = True
         Me.TextBox2.Name = "TextBox2"
         Me.TextBox2.Size = New System.Drawing.Size(227, 183)
         Me.TextBox2.TabIndex = 2
+        Me.TextBox2.Visible = False
         '
         'openFD
         '
         Me.openFD.FileName = "OpenFileDialog1"
         '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.0!)
+        Me.Label1.Location = New System.Drawing.Point(32, 50)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(90, 29)
+        Me.Label1.TabIndex = 3
+        Me.Label1.Text = "Source"
+        Me.Label1.Visible = False
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.0!)
+        Me.Label2.Location = New System.Drawing.Point(337, 50)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(84, 29)
+        Me.Label2.TabIndex = 4
+        Me.Label2.Text = "Target"
+        Me.Label2.Visible = False
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.PictureBox1.Image = CType(resources.GetObject("PictureBox1.Image"), System.Drawing.Image)
+        Me.PictureBox1.Location = New System.Drawing.Point(32, 303)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(104, 104)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize
+        Me.PictureBox1.TabIndex = 5
+        Me.PictureBox1.TabStop = False
+        '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.ClientSize = New System.Drawing.Size(619, 499)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
         Me.Controls.Add(Me.TextBox2)
         Me.Controls.Add(Me.TextBox1)
         Me.Controls.Add(Me.MenuStrip1)
@@ -216,6 +259,7 @@ Partial Class Form1
         Me.Text = "Form1"
         Me.MenuStrip1.ResumeLayout(False)
         Me.MenuStrip1.PerformLayout()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
@@ -231,16 +275,19 @@ Partial Class Form1
     Friend WithEvents NewProjectToolStripMenuItem As ToolStripMenuItem
     Friend WithEvents NewFileToolStripMenuItem As ToolStripMenuItem
     Friend WithEvents EditToolStripMenuItem As ToolStripMenuItem
-    Friend WithEvents UndoToolStripMenuItem As ToolStripMenuItem
-    Friend WithEvents CutToolStripMenuItem As ToolStripMenuItem
-    Friend WithEvents CopyToolStripMenuItem As ToolStripMenuItem
-    Friend WithEvents PasteToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents menuUndo As ToolStripMenuItem
+    Friend WithEvents menuCut As ToolStripMenuItem
+    Friend WithEvents menuCopy As ToolStripMenuItem
+    Friend WithEvents menuPaste As ToolStripMenuItem
     Friend WithEvents ViewToolStripMenuItem As ToolStripMenuItem
-    Friend WithEvents ViewTextBoxesToolStripMenuItem As ToolStripMenuItem
-    Friend WithEvents ViewLabelsToolStripMenuItem As ToolStripMenuItem
-    Friend WithEvents ViewImagesToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents menuViewTextBoxes As ToolStripMenuItem
+    Friend WithEvents menuViewLabels As ToolStripMenuItem
+    Friend WithEvents menuViewImages As ToolStripMenuItem
     Friend WithEvents TextBox1 As TextBox
     Friend WithEvents TextBox2 As TextBox
     Friend WithEvents openFD As OpenFileDialog
     Friend WithEvents saveFD As SaveFileDialog
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents PictureBox1 As PictureBox
 End Class
--- 5a_Menus/Form1.vb
+++ 5a_Menus/Form1.vb
@@ -45,4 +45,45 @@
             MessageBox.Show(strFileName)
         End If
     End Sub
+
+    Private Sub menuCopy_Click(sender As Object, e As EventArgs) Handles menuCopy.Click
+        TextBox1.Copy()
+    End Sub
+
+    Private Sub menuPaste_Click(sender As Object, e As EventArgs) Handles menuPaste.Click
+        TextBox2.Paste()
+    End Sub
+
+    Private Sub menuCut_Click(sender As Object, e As EventArgs) Handles menuCut.Click
+        TextBox1.Cut()
+    End Sub
+
+    Private Sub menuUndo_Click(sender As Object, e As EventArgs) Handles menuUndo.Click
+        TextBox1.Undo()
+    End Sub
+
+
+    Private Sub menuViewTextBoxes_Click(sender As Object, e As EventArgs) Handles menuViewTextBoxes.Click
+        menuViewTextBoxes.Checked = Not menuViewTextBoxes.Checked
+
+        If menuViewTextBoxes.Checked = True Then
+            TextBox1.Visible = True
+            TextBox2.Visible = True
+        Else
+            TextBox1.Visible = False
+            TextBox2.Visible = False
+        End If
+    End Sub
+
+    Private Sub menuViewLabels_Click(sender As Object, e As EventArgs) Handles menuViewLabels.Click
+        menuViewLabels.Checked = Not menuViewLabels.Checked
+
+        If menuViewLabels.Checked = True Then
+            Label1.Visible = True
+            Label2.Visible = True
+        Else
+            Label1.Visible = False
+            Label2.Visible = False
+        End If
+    End Sub
 End Class
